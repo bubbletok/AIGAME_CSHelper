f9de2c42fa46552acfd733571421548ac03a7012
Sa1ntpark <123047501+Sa1ntpark@users.noreply.github.com>
2024-06-09 19:38:22 +0900

    Delete Source directory

--- Source/text_recognintion.py
+++
@@ -1,6 +0,0 @@
-def text_recognition(x1,y1,x2,y2):
-
-    image = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(x1,y1,x2,y2))), cv2.COLOR_BGR2RGB)
-    img2char = pytesseract.image_to_string(image)
-
-    return img2char
