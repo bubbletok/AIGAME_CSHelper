fea6bc9186a0802daf789bb21609b429d8dcb0b3
richardlee-kr <richardl22245@gmail.com>
2024-06-10 00:23:30 +0900

    [FIX] color format unmatched

--- Source/HPBarProcessor.py
+++ Source/HPBarProcessor.py
@@ -1,7 +1,7 @@
 import PIL.Image
 
-targetColor = (255, 0, 0, 255)
-black = (0,0,0,255)
+targetColor = (255, 0, 0)
+black = (0,0,0)
 
 def is_similar_color(color1, color2, threshold=200):
     return all(abs(c1 - c2) < threshold for c1, c2 in zip(color1, color2))
@@ -13,11 +13,13 @@ def SimplifyImage(img:PIL.Image) -> PIL.Image:
     for y in range(bar.height):
         for x in range(bar.width):
             current_color = pixels[x, y]
-            if is_similar_color(current_color, targetColor):
+            if is_similar_color(current_color[0:3], targetColor):
                 pixels[x, y] = targetColor
             else:
                 pixels[x, y] = black
 
+    #bar.show()
+    
     return bar
 
 def HPBarOutliner(img:PIL.Image) -> PIL.Image:
@@ -27,7 +29,7 @@ def HPBarOutliner(img:PIL.Image) -> PIL.Image:
     for y in range(img.height):
         for x in range(img.width):
             current_color = pixels[x, y]
-            if is_similar_color(current_color, black, 20):
+            if is_similar_color(current_color[0:3], black, 20):
                 pixels[x, y] = black
 
     hpbarTopLeft = FindTopLeft(img)
@@ -42,19 +44,28 @@ def HPBarOutliner(img:PIL.Image) -> PIL.Image:
 
 def FindTopLeft(img:PIL.Image) -> list[int,int]:
     pixels = img.load()
+    count = 0
+    result = (0,0)
 
     for y in range(img.height):
         for x in range(img.width):
+            if count > img.width/2:
+                return result
             current_color = pixels[x, y]
-            if current_color == black[0:3]:
-                return (x,y)
+            if current_color[0:3] == black:
+                if count == 0:
+                    result = (x,y)
+                count += 1
+            elif current_color[0:3] != black and count < img.width/2:
+                count = 0
+
 def FindBottomRight(img:PIL.Image) -> list[int,int]:
     pixels = img.load()
 
     for y in range(img.height-1, -1, -1):
         for x in range(img.width-1, -1, -1):
             current_color = pixels[x,y]
-            if current_color == black[0:3]:
+            if current_color[0:3] == black:
                 return (x,y)
 
 def CaculateHpRatio(image:PIL.Image) -> float:
@@ -80,19 +91,19 @@ def CaculateHpRatio(image:PIL.Image) -> float:
 def CheckBarStart(img:PIL.Image) -> int:
     for x in range(img.width):
         checkColor = img.getpixel((x,img.height/2))
-        if checkColor == targetColor[0:3]:
+        if checkColor[0:3] == targetColor:
             return x
 
 def CheckHPEnd(img:PIL.Image, offset:int) -> int:
     for x in range(offset, img.width):
         checkColor = img.getpixel((x,img.height/2))
-        if checkColor == black[0:3]:
+        if checkColor[0:3] == black:
             return x
 
 
 '''Test'''
-test = PIL.Image.open("Data/HPBar_test.png")
-crop = SimplifyImage(test)
-#crop.show()
-print(CaculateHpRatio(crop))
-crop.save("Data/HPBar_processed.png")
+if __name__ == '__main__':
+    test = PIL.Image.open("Data/HPBar_test.png")
+    crop = SimplifyImage(test)
+    print(CaculateHpRatio(test))
+    crop.save("Data/HPBar_processed.png")
